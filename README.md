b87bf5ea098dbef7d7d533e0c84c75409e97ae3b
David Capello <davidcapello@gmail.com>
2020-10-22 21:35:50 -0300

    First version

---
+++ README.md
@@ -0,0 +1,159 @@
+# export-aseprite-file
+
+> by David Capello
+
+This is a script that can be used to export the data from a
+`.aseprite` file into a JSON + a collection of PNG files.  This works
+with Aseprite v1.2.25 and support the future v1.3 to export
+tilemap/tileset data.
+
+## Example
+
+Usage:
+
+    aseprite -b map.aseprite -script export.lua
+
+In this example `export.lua` will create a folder named `map` with
+some files inside:
+
+```
+map/
+  sprite.json
+  image1.png
+  image2.png
+  tileset1.png
+```
+
+An example of `map/sprite.json` content:
+
+```
+{
+  "filename": "map.aseprite",
+  "width": 32,
+  "height": 32,
+  "frames": [
+    { "duration": 0.1 },
+    { "duration": 0.15 }
+  ],
+  "layers": [
+    {
+      "name": "Group 1",
+      "layers": [
+        {
+          "name": "Common Layer",
+          "cels": [
+            {
+              "bounds": { "x": 10, "y": 13, "width": 12, "height": 13 },
+              "frame": 0,
+              "image": "map/image1.png"
+            },
+            {
+              "bounds": { "x": 6, "y": 15, "width": 12, "height": 12 },
+              "frame": 1,
+              "image": "map/image2.png"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name": "Tilemap Layer",
+      "cels": [
+        {
+          "bounds": { "x": 0, "y": 0, "width": 32, "height": 32 },
+          "data": "text1",
+          "frame": 0,
+          "tilemap": {
+            "width": 4,
+            "height": 4,
+            "tiles": [
+              0, 1, 2, 3,
+              4, 5, 5, 6,
+              7, 5, 5, 8,
+              9, 10, 11, 12
+            ]
+          }
+        },
+        {
+          "bounds": { "x": 1, "y": 1, "width": 32, "height": 32 },
+          "frame": 1,
+          "color": "#f7a547",
+          "data": "text2",
+          "tilemap": {
+            "width": 4,
+            "height": 4,
+            "tiles": [
+              0, 1, 1, 3,
+              4, 4, 4, 8,
+              4, 4, 4, 8,
+              9, 10, 10, 12
+            ]
+          }
+        }
+      ]
+    }
+  ],
+  "tilesets": [
+    {
+      "grid": {
+        "tileSize": { "width": 8, "height": 8 }
+      },
+      "image": "map/tileset1.png"
+    }
+  ],
+  "tags": [
+    {
+      "name": "a",
+      "aniDir": "pingpong",
+      "color": "#000000",
+      "from": 0,
+      "to": 2
+    },
+    {
+      "name": "b",
+      "aniDir": "forward",
+      "color": "#000000",
+      "from": 0,
+      "to": 1
+    },
+    {
+      "name": "c",
+      "aniDir": "reverse",
+      "color": "#000000",
+      "from": 1,
+      "to": 2
+    }
+  ],
+  "slices": [
+    {
+      "name": "Slice 1",
+      "color": "#0000ff",
+      "bounds": { "x": 4, "y": 19, "width": 8, "height": 6 }
+    },
+    {
+      "name": "Slice 2",
+      "color": "#0000ff",
+      "bounds": { "x": 14, "y": 9, "width": 9, "height": 11 },
+      "center": { "x": 1, "y": 1, "width": 7, "height": 9 }
+    },
+    {
+      "name": "Slice 3",
+      "color": "#0000ff",
+      "data": "text3",
+      "bounds": { "x": 17, "y": 23, "width": 8, "height": 7 },
+      "pivot": { "x": 4, "y": 2 }
+    }
+  ]
+}
+```
+
+## Acknowledges
+
+This project uses [json.lua](https://github.com/rxi/json.lua) by
+[rxi](https://github.com/rxi) to export Lua tables to JSON files.
+
+## License
+
+This code is distributed under the terms of the MIT license. You can
+use this code for your own purpose to export the specific data that
+you need.
---
+++ export.lua
@@ -0,0 +1,231 @@
+-- export.lua
+-- Copyright (C) 2020  David Capello
+--
+-- This file is released under the terms of the MIT license.
+
+local spr = app.activeSprite
+if not spr then return print "No active sprite" end
+
+if ColorMode.TILEMAP == nil then ColorMode.TILEMAP = 4 end
+assert(ColorMode.TILEMAP == 4)
+
+local fs = app.fs
+local pc = app.pixelColor
+local output_folder = fs.fileTitle(spr.filename)
+local image_n = 0
+local tileset_n = 0
+
+local function write_json_data(filename, data)
+  local json = dofile('./json.lua')
+  local file = io.open(filename, "w")
+  file:write(json.encode(data))
+  file:close()
+end
+
+local function fill_user_data(t, obj)
+  if obj.color.alpha > 0 then
+    if obj.color.alpha == 255 then
+      t.color = string.format("#%02x%02x%02x",
+                              obj.color.red,
+                              obj.color.green,
+                              obj.color.blue)
+    else
+      t.color = string.format("#%02x%02x%02x%02x",
+                              obj.color.red,
+                              obj.color.green,
+                              obj.color.blue,
+                              obj.color.alpha)
+    end
+  end
+  if pcall(function() return obj.data end) then -- a tag doesn't have the data field pre-v1.3
+    if obj.data and obj.data ~= "" then
+      t.data = obj.data
+    end
+  end
+end
+
+local function export_tileset(tileset)
+  local t = {}
+  local grid = tileset.grid
+  local size = grid.tileSize
+  t.grid = { tileSize={ width=grid.tileSize.width, height=grid.tileSize.height } }
+  if #tileset > 0 then
+    local spec = spr.spec
+    spec.width = size.width
+    spec.height = size.height * #tileset
+    local image = Image(spec)
+    image:clear()
+    for i = 0,#tileset-1 do
+      local tile = tileset:getTile(i)
+      image:drawImage(tile, 0, i*size.height)
+    end
+
+    tileset_n = tileset_n + 1
+    local imageFn = fs.joinPath(output_folder, "tileset" .. tileset_n .. ".png")
+    image:saveAs(imageFn)
+    t.image = imageFn
+  end
+  return t
+end
+
+local function export_tilesets(tilesets)
+  local t = {}
+  for _,tileset in ipairs(tilesets) do
+    table.insert(t, export_tileset(tileset))
+  end
+  return t
+end
+
+local function export_frames(frames)
+  local t = {}
+  for _,frame in ipairs(frames) do
+    table.insert(t, { duration=frame.duration })
+  end
+  return t
+end
+
+local function export_cel(cel)
+  local t = {
+    frame=cel.frameNumber-1,
+    bounds={ x=cel.bounds.x,
+             y=cel.bounds.y,
+             width=cel.bounds.width,
+             height=cel.bounds.height }
+  }
+
+  if cel.image.colorMode == ColorMode.TILEMAP then
+    local tilemap = cel.image
+    -- save tilemap
+    t.tilemap = { width=tilemap.width,
+                  height=tilemap.height,
+                  tiles={} }
+    for it in tilemap:pixels() do
+      table.insert(t.tilemap.tiles, pc.tileI(it()))
+    end
+  else
+    -- save regular cel
+    image_n = image_n + 1
+    local imageFn = fs.joinPath(output_folder, "image" .. image_n .. ".png")
+    cel.image:saveAs(imageFn)
+    t.image = imageFn
+  end
+
+  fill_user_data(t, cel)
+  return t
+end
+
+local function export_cels(cels)
+  local t = {}
+  for _,cel in ipairs(cels) do
+    table.insert(t, export_cel(cel))
+  end
+  return t
+end
+
+local function export_layer(layer, export_layers)
+  local t = { name=layer.name }
+  if layer.isImage then
+    if layer.opacity < 255 then
+      t.opacity = layer.opacity
+    end
+    if layer.blendMode ~= BlendMode.NORMAL then
+      t.blendMode = layer.blendMode
+    end
+    if #layer.cels >= 1 then
+      t.cels = export_cels(layer.cels)
+    end
+  elseif layer.isGroup then
+    t.layers = export_layers(layer.layers)
+  end
+  fill_user_data(t, layer)
+  return t
+end
+
+local function export_layers(layers)
+  local t = {}
+  for _,layer in ipairs(layers) do
+    table.insert(t, export_layer(layer, export_layers))
+  end
+  return t
+end
+
+local function ani_dir(d)
+  local values = { "forward", "reverse", "pingpong" }
+  return values[d+1]
+end
+
+local function export_tag(tag)
+  local t = {
+    name=tag.name,
+    from=tag.fromFrame.frameNumber-1,
+    to=tag.toFrame.frameNumber-1,
+    aniDir=ani_dir(tag.aniDir)
+  }
+  fill_user_data(t, tag)
+  return t
+end
+
+local function export_tags(tags)
+  local t = {}
+  for _,tag in ipairs(tags) do
+    table.insert(t, export_tag(tag, export_tags))
+  end
+  return t
+end
+
+local function export_slice(slice)
+  local t = {
+    name=slice.name,
+    bounds={ x=slice.bounds.x,
+             y=slice.bounds.y,
+             width=slice.bounds.width,
+             height=slice.bounds.height }
+  }
+  if slice.center then
+    t.center={ x=slice.center.x,
+               y=slice.center.y,
+               width=slice.center.width,
+               height=slice.center.height }
+  end
+  if slice.pivot then
+    t.pivot={ x=slice.pivot.x,
+               y=slice.pivot.y }
+  end
+  fill_user_data(t, slice)
+  return t
+end
+
+local function export_slices(slices)
+  local t = {}
+  for _,slice in ipairs(slices) do
+    table.insert(t, export_slice(slice, export_slices))
+  end
+  return t
+end
+
+----------------------------------------------------------------------
+-- Creates output folder
+
+fs.makeDirectory(output_folder)
+
+----------------------------------------------------------------------
+-- Write /sprite.json file in the output folder
+
+local jsonFn = fs.joinPath(output_folder, "sprite.json")
+local data = {
+  filename=spr.filename,
+  width=spr.width,
+  height=spr.height,
+  frames=export_frames(spr.frames),
+  layers=export_layers(spr.layers)
+}
+if #spr.tags > 0 then
+  data.tags = export_tags(spr.tags)
+end
+if #spr.slices > 0 then
+  data.slices = export_slices(spr.slices)
+end
+if pcall(function() return spr.tilesets end) then
+  data.tilesets = export_tilesets(spr.tilesets)
+end
+write_json_data(jsonFn, data)
